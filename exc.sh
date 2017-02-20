ad9b3620e2e02ab867d2ec8e7d4e7d94f42e6a1d
shun-sa <saku0aab@gmail.com>
2017-02-20 11:45:02 +0900

    update file procedure

--- exc.sh
+++ exc.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-# cat $1>$2
-awk '{ t = $1; $1 = $2; $2 = t; print; }' "$1" > "$2"
+CMDNAME=`basename $0`
+if [ $# -ne 2 ]; then
+  echo "Usage: $CMDNAME file1 file2" 1>&2
+  exit 1
+fi
+
+awk '{ t = $1; $1 = $2; $2 = t; print; }' "$1" > .temp
+cat .temp >"$2"
+rm .temp
