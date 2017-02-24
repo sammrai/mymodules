7195f424ddd9219035dbbe6537f95f0502fe01b6
shun-sa <saku0aab@gmail.com>
2017-02-24 16:41:56 +0900

    fix document

--- reg/Lasso.py
+++ reg/Lasso.py
@@ -2,44 +2,6 @@ import numpy as np
 from scipy import optimize as opt
 from sklearn import linear_model
 
-def _preprocess_data(X, y, fit_intercept, normalize=False, copy=True, sample_weight=None, return_mean=False):
-    """
-    Centers data to have mean zero along axis 0. If fit_intercept=False or if
-    the X is a sparse matrix, no centering is done, but normalization can still
-    be applied. The function returns the statistics necessary to reconstruct
-    the input data, which are X_offset, y_offset, X_scale, such that the output
-        X = (X - X_offset) / X_scale
-    X_scale is the L2 norm of X - X_offset. If sample_weight is not None,
-    then the weighted mean of X and y is zero, and not the mean itself. If
-    return_mean=True, the mean, eventually weighted, is returned, independently
-    of whether X was centered (option used for optimization with sparse data in
-    coordinate_descend).
-    This is here because nearly all linear models will want their data to be
-    centered.
-    """
-
-    # X = check_array(X, copy=copy, accept_sparse=['csr', 'csc'],
-    #                 dtype=FLOAT_DTYPES)
-
-    if fit_intercept:
-        X_offset = np.average(X, axis=0, weights=sample_weight)
-        X -= X_offset
-        if normalize:
-            X, X_scale = f_normalize(X, axis=0, copy=False,
-                                     return_norm=True)
-        else:
-            X_scale = np.ones(X.shape[1])
-
-        y_offset = np.average(y, axis=0, weights=sample_weight)
-        y = y - y_offset
-    else:
-        X_offset = np.zeros(X.shape[1])
-        X_scale = np.ones(X.shape[1])
-        y_offset = 0. if y.ndim == 1 else np.zeros(y.shape[1], dtype=X.dtype)
-
-    return X, y, X_offset, y_offset, X_scale
-
-
 class Lasso(linear_model.LinearRegression):
     """
     Implementation of lasso using ADMM.
@@ -50,17 +12,17 @@ class Lasso(linear_model.LinearRegression):
     Parameters
     ----------
     iter_num : int
-        The number of ADMM iteration
+        The number of ADMM iteration.
     alpha : float
-        If None, alphas are set automatically
+        Regularization parameter.
     rho : float
         Parameters of convergence stability. This considered to be l2 norm regularization parameter.
     omega : float
         Parameters of convergence acceleration.
     solver : default "liblinear"
-        Type of solver. See here https://docs.scipy.org/doc/scipy-0.18.1/reference/generated/scipy.optimize.minimize.html#scipy.optimize.minimize
+        Type of solver. See uel here https://docs.scipy.org/doc/scipy-0.18.1/reference/generated/scipy.optimize.minimize.html#scipy.optimize.minimize
     verbose : boolean
-        Print debug message
+        Print debug message.
 
     Attributes
     ----------
